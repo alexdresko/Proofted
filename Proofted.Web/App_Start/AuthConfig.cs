c9ca67367ab6d479a3995c04c8bb8903f1063c22
Alex Dresko <me@alexdresko.com>
2012-10-02 22:25:19 -0400

    Google works...

--- Proofted.Web/App_Start/AuthConfig.cs
+++ Proofted.Web/App_Start/AuthConfig.cs
@@ -26,7 +26,7 @@ namespace Proofted.Web
             //    appId: "",
             //    appSecret: "");
 
-            //OAuthWebSecurity.RegisterGoogleClient();
+            OAuthWebSecurity.RegisterGoogleClient();
         }
     }
 }
